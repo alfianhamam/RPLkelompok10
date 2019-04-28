7a4e613e3124dd9f42a27cc2f06935a8a5f7d358
alfianhamam <hamam_18@apps.ipb.ac.id>
2019-04-28 09:08:12 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'event', loadChildren: './event/event.module#EventPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
